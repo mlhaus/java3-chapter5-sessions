7ad534629aeb4be1f5234e99cd6e9f8b0856986c
Marc Hauschildt <marc.hauschildt@gmail.com>
2021-05-06 13:02:48 -0500

    Initial commit

---
+++ src/main/java/com/hauschildt/User.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hauschildt;
+
+import java.io.Serializable;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class User implements Serializable {
+    private long userId;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+    private Map<String, Boolean> permissions;
+
+    public User() {
+        this.userId = 0L;
+        this.username = "";
+        this.firstName = "";
+        this.lastName = "";
+        permissions = new Hashtable<>();
+    }
+
+    public User(long userId, String username, String firstName, String lastName, Map<String, Boolean> permissions) {
+        this.userId = userId;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.permissions = permissions;
+    }
+
+    
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map<String, Boolean> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Map<String, Boolean> permissions) {
+        this.permissions = permissions;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", firstName=" + firstName + ", lastName=" + lastName + '}';
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    
+}
